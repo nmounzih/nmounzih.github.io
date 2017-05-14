4b1ce1b32a3acdb471134ca6f547a1f081e68a63
Nadia Mounzih <nmounzih@yahoo.com>
2017-05-14 09:52:14 -0400

    Changed background photo

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
 # nmounzih.github.io
 
 This is my portfolio site. Currently under construction.
-
-
-## Acknowledgments
-
-* Background photo of Bangkok comes from  https://matadornetwork.com/trips/40-worlds-impressive-skylines/2/
--- portfolio.js
+++ portfolio.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
   $('#linkedIn').mouseenter(function(){
     $(this).animate({
-      height: '+=11px'
+      height: '+=5px'
     });
   });
   $('#linkedIn').mouseleave(function(){
     $(this).animate({
-      height: '-=11px'
+      height: '-=5px'
     });
   });
 });
@@ -14,12 +14,12 @@ $(document).ready(function(){
 $(document).ready(function(){
   $('#gitHub').mouseenter(function(){
     $(this).animate({
-      height: '+=10px'
+      height: '+=5px'
     });
   });
   $('#gitHub').mouseleave(function(){
     $(this).animate({
-      height: '-=10px'
+      height: '-=5px'
     });
   });
 });
@@ -27,12 +27,12 @@ $(document).ready(function(){
 $(document).ready(function(){
   $('#email').mouseenter(function(){
     $(this).animate({
-      height: '+=10px'
+      height: '+=5px'
     });
   });
   $('#email').mouseleave(function(){
     $(this).animate({
-      height: '-=10px'
+      height: '-=5px'
     });
   });
 });
